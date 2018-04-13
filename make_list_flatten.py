71bd784aa35a7ea2d9512c4dbcd3705a1133d182
oplesovskykh <oplesovskykh@vidscale.com>
2018-04-13 11:54:25 +0300

    cycle

--- make_list_flatten.py
+++ make_list_flatten.py
@@ -3,11 +3,26 @@
 # to
 # lst = [1, 2, 3, 4, 6, 7]
 
-list = [1, [2, 3], 4, [6, 7]]
-lis = tuple(list[1]) + tuple(list[3])
-list.pop(1)
-list.pop(2)
-list.extend(lis)
-list.sort()
-print(list)
+list = [1, [2, 3], 4, [[6, 7]]]
+list_flatten = []
+list2 = []
+list3 = []
+list4 = []
+
+for i in list:
+    if type(i) == int:
+        list_flatten.append(i)
+    else:
+            list2 += tuple(i)
+list_flatten = sorted(list_flatten + list2)
+for i in list_flatten:
+    if type(i) == int:
+        list3.append(i)
+    else:
+        list4 += i
+list_flatten = sorted(list3 + list4)
+print(list_flatten)
+
+
+
 
